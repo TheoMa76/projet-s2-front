54bb1040af5c1c25467b6757f6fece8a928f97f4
Theo Maerten <theo.maerten@sga-industires.com>
2024-07-12 07:59:14 +0200

    Revert "Merge branch 'pc-fixe' of https://github.com/TheoMa76/projet-s2-front into pc-boulot"
    This reverts commit 690a085150b1e058ff33f6c2abcf33a517dcae42, reversing
    changes made to ca53eae808db0c25f6d6c3011f97e132504b35fd.

--- src/components/atoms/Texts/Title/AwesomeTitle.tsx
+++ src/components/atoms/Texts/Title/AwesomeTitle.tsx
@@ -12,9 +12,11 @@ const imgSrc = '/img/EasyMod.png';
 
 const AwesomeTitle: React.FC<MinecraftHNProps> = ({ className,children }) => {
   return (
-    <div className="relative w-2/4 h-1/4">
+    <div className={`relative w-fit h-1/4 ${className}`}>
       <img src={imgSrc} alt="easymod" className="w-full h-auto" />
-      <MinecraftHN as='h1' className="text-yellow-300 text-3xl absolute bottom-20 right-10 diagonal-text breath-animation">{children}</MinecraftHN>
+      <MinecraftHN as='h2' className="text-yellow-300 text-xs sm:text-lg md:bottom-0 lg:bottom-3 lg:-right-10 xl:bottom-3 xl:-right-32 md:text-xl lg:text-2xl xl:text-3xl absolute bottom-0 right-0 m-2 diagonal-text breath-animation">
+        {children}
+      </MinecraftHN>
     </div>
   );
 }
